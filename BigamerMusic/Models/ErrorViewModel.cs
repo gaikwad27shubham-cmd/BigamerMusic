d3fdfe5f2babe203e2f5b759ee139dbd9a590048
gaikwad27shubham-cmd <gaikwad27shubham@gmail.com>
2025-10-20 18:01:09 +0530

    upload3

--- BigamerMusic/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BigamerMusic.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
