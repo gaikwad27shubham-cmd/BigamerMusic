9289e660f3b9d9f31e047eeb61a0ceb21adf94a9
gaikwad27shubham-cmd <gaikwad27shubham@gmail.com>
2025-10-20 17:35:49 +0530

    file uploaded

---
+++ BigamerMusic/Models/ActionModal.cs
@@ -0,0 +1,16 @@
+﻿namespace BigamerMusic.Models
+{
+    public class ActionModal
+    {
+        // Input Parameters
+        public string ProcName { get; set; }            // Stored procedure name
+        public string formXml { get; set; }             // JSON or XML input
+        public int PrimaryId { get; set; }              // Input Primary ID
+        public int UserID { get; set; }                 // User identifier
+        public int RoleID { get; set; }                 // Role identifier
+
+        // Output Parameters
+        public int MessageStatusId { get; set; }        // Output Message ID (parsed from @MessageID)
+        public string MessageText { get; set; }
+    }
+}
---
+++ BigamerMusic/Services/IActionRepository.cs
@@ -0,0 +1,10 @@
+﻿using BigamerMusic.Models;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Threading.Tasks;
+namespace BigamerMusic.Services
+{
+    public interface IActionRepository
+    {
+        Task<ActionModal> ExecuteProcedureAsync(ActionModal modal);
+    }
+}
